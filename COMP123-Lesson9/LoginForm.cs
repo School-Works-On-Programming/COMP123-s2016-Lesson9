f2491418043314ae38fabc1dead588266b31aae6
R.SIRHC <christopher.r99@live.com>
2016-07-12 13:55:15 -0400

    Fixed error

--- COMP123-Lesson9/LoginForm.cs
+++ COMP123-Lesson9/LoginForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,50 +13,53 @@ namespace COMP123_Lesson9
 {
     public partial class LoginForm : Form
     {
+        /**
+         * <summary>
+         * This is the default constructor for the LoginForm Class
+         * </summary>
+         * 
+         * @constructor LoginForm
+         */
         public LoginForm()
         {
-            /**
-             * <summary>
-             * 
-             * </summary>
-             **/
             InitializeComponent();
         }
 
 
-        //PRIVATE METHODS +++++++++++++++++++++++++++++++++++++++++
-
-            /**
-             *<summary>
-             * This mehtod cleras the UsernameTExtBox and the PasswordTextBox 
-             * </summary> 
-             * 
-             * @private
-             * @method _clearForm
-             * @returns {void}
-             **/
-        private void _clearForm()
-        {
-            UsernameTextBox.Clear();
-            PasswordTextBox.Clear(); 
-        }
+        // PRIVATE METHODS +++++++++++++++++++++++++++++++++++++++++++++++
 
         /**
          * <summary>
-         * This is the LoginButton click even handler 
+         * This method clears the UsernameTextBox and the PasswordTextBox
+         * </summary>
+         * 
+         * @private
+         * @method _clearForm
+         * @returns {void}
+         */
+        private void _clearForm()
+        {
+            UsernameTextBox.Clear();
+            PasswordTextBox.Clear();
+        }
+
+
+        /**
+         * <summary>
+         * This is the LoginButton Click event handler
          * </summary>
          * 
          * @private
          * @method LoginButton_Click
          * @param {object} sender
-         * @param {EventArgs} e 
+         * @param {EventArgs} e
          * @returns {void}
          */
         private void LoginButton_Click(object sender, EventArgs e)
         {
             Debug.WriteLine("Username: " + UsernameTextBox.Text);
             Debug.WriteLine("Password: " + PasswordTextBox.Text);
-            this._clearForm(); 
+            this._clearForm();
         }
     }
 }
